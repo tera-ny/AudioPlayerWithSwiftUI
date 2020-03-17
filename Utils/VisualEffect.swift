049e16e14145f92de1d3428ba71eb21493f44721
g4zeru <haru25haru02@gmail.com>
2020-03-17 23:09:58 +0900

    fix visual effect

--- Utils/VisualEffect.swift
+++ Utils/VisualEffect.swift
@@ -6,4 +6,27 @@
 //  Copyright © 2020 g4zeru. All rights reserved.
 //
 
-import Foundation
+import SwiftUI
+
+struct VisualEffect {
+    private var _effect: UIVisualEffect?
+    private init(effect: UIVisualEffect) {
+        _effect = effect
+    }
+}
+
+extension VisualEffect {
+    static func blurEffect(style: UIBlurEffect.Style) -> Self {
+        VisualEffect(effect: UIBlurEffect(style: style))
+    }
+}
+
+extension VisualEffect: UIViewRepresentable {
+    func makeUIView(context _: Context) -> UIVisualEffectView {
+        return UIVisualEffectView(effect: _effect)
+    }
+
+    func updateUIView(_ uiView: UIVisualEffectView, context _: Context) {
+        uiView.effect = _effect
+    }
+}
